ec91fea9aaa8395f9ceebd837f3e09e021fe0b7f
Golu Prajapati <golupraja96@gmail.com>
2025-02-21 14:35:55 +0530

    all fix

--- src/app/login-page/login-page.component.ts
+++ src/app/login-page/login-page.component.ts
@@ -1,6 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { ReactiveFormsModule } from '@angular/forms';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import {MatSelectModule} from '@angular/material/select';
 import {MatInputModule} from '@angular/material/input';
 import {MatFormFieldModule} from '@angular/material/form-field';
@@ -13,13 +13,33 @@ import {MatFormFieldModule} from '@angular/material/form-field';
   styleUrl: './login-page.component.css'
 })
 export class LoginPageComponent {
-  constructor(){
-    this.generateCaptcha();
+  form!: FormGroup
+  constructor(private _fb: FormBuilder){
+    this.form = this._fb.group({
+      userName: ['', Validators.required],
+      password: ['', Validators.required],
+      captcha: ['', Validators.required]
+    })
+    // this.generateCaptcha();
   }
-  captchaUrl: any;
+  captchaUrl: string = 'dGoOz';
 
   generateCaptcha(): void {
     const chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
     this.captchaUrl = Array.from({ length: 5 }, () => chars[Math.floor(Math.random() * chars.length)]).join('');
+  }
+
+  submitForm(){
+    if(this.form.valid){
+      if(this.captchaUrl === this.form.get('captcha')?.value){
+        console.log(this.form.value)
+      }else{
+        alert('please enter valid captcha.')
+      }
+      
+    }else{
+      alert('Please fill all required fields!');
+      this.form.markAllAsTouched();
+    }
   }
 }
